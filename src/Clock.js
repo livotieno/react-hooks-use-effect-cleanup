8e0b9385f0e35c1064ac87c809a5f78061141980
livotieno <livio5261@gmail.com>
2023-11-02 10:28:33 +0300

    done

--- src/Clock.js
+++ src/Clock.js
@@ -3,13 +3,20 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
+  // use cleanup funtion to prevent memory leaks/infinite loop
   useEffect(() => {
-    setInterval(() => {
+    let intervalId = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    // cleanup function
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
 }
 
 export default Clock;
+
